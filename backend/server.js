36e4c18d6e1974d1aa21914a666f46c4fdce1e22
Zach <zacharyzwilson@outlook.com>
2023-09-02 01:57:27 -0500

    test

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require("dotenv").config(); // Load environment variables from .env file
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
+const path = require("path"); // NEW: Require path module
 const config = require("./db/config");
 
 const app = express();
@@ -27,8 +28,17 @@ app.use("/lists", listRoutes);
 const plantRoutes = require("./routes/plants");
 app.use("/lists", plantRoutes); // This will cover the nested plants routes, e.g., /lists/:listId/plants
 
-const perenualRoutes = require("./api/perenual");
-app.use("/api/perenual", perenualRoutes);
+const perennialRoutes = require("./api/perenual"); // Ensure this path is correct.
+app.use("/api/perenual", perennialRoutes);
+
+// NEW: Serve static files from the React frontend app
+const frontendBuildPath = path.join(__dirname, "..", "frontend", "build");
+app.use(express.static(frontendBuildPath));
+
+// NEW: Catch-all handler for any unmatched route
+app.get("*", (req, res) => {
+	res.sendFile(path.join(frontendBuildPath, "index.html"));
+});
 
 // Error handling
 app.use((err, req, res, next) => {
