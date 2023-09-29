a932593a3f273fb6b91de1eba8b6b564a2059005
Zane <zaneerebos@gmail.com>
2023-09-29 08:08:26 -0500

    schema update

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,257 +1,49 @@
---
--- PostgreSQL database dump
---
+-- Create sequence for users table
+CREATE SEQUENCE users_id_seq START WITH 1 INCREMENT BY 1;
 
--- Dumped from database version 14.9 (Ubuntu 14.9-0ubuntu0.22.04.1)
--- Dumped by pg_dump version 14.9 (Ubuntu 14.9-0ubuntu0.22.04.1)
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: listplants; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.listplants (
-    id integer NOT NULL,
-    list_id integer,
-    plant_id integer
+-- Create users table
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY DEFAULT nextval('users_id_seq'),
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE
 );
 
+-- Create sequence for userlists table
+CREATE SEQUENCE userlists_id_seq START WITH 1 INCREMENT BY 1;
 
---
--- Name: listplants_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.listplants_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: listplants_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.listplants_id_seq OWNED BY public.listplants.id;
-
-
---
--- Name: plants; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.plants (
-    id integer NOT NULL,
-    common_name character varying(255) NOT NULL,
-    scientific_name character varying(255) NOT NULL,
-    watering character varying(255) NOT NULL,
-    sunlight character varying(255) NOT NULL,
-    description text,
-    zoning character varying(255) NOT NULL,
-    icon_path character varying(255) NOT NULL
+-- Create userlists table
+CREATE TABLE userlists (
+    id INTEGER PRIMARY KEY DEFAULT nextval('userlists_id_seq'),
+    user_id INTEGER,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+-- Create sequence for plants table
+CREATE SEQUENCE plants_id_seq START WITH 1 INCREMENT BY 1;
 
---
--- Name: plants_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.plants_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: plants_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.plants_id_seq OWNED BY public.plants.id;
-
-
---
--- Name: userlists; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.userlists (
-    id integer NOT NULL,
-    user_id integer,
-    name character varying(255) NOT NULL,
-    description text
+-- Create plants table
+CREATE TABLE plants (
+    id INTEGER PRIMARY KEY DEFAULT nextval('plants_id_seq'),
+    common_name VARCHAR(255) NOT NULL,
+    scientific_name VARCHAR(255) NOT NULL,
+    watering VARCHAR(255) NOT NULL,
+    sunlight VARCHAR(255) NOT NULL,
+    description TEXT,
+    zoning VARCHAR(255) NOT NULL,
+    icon_path VARCHAR(255) NOT NULL
 );
 
+-- Create sequence for listplants table
+CREATE SEQUENCE listplants_id_seq START WITH 1 INCREMENT BY 1;
 
---
--- Name: userlists_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.userlists_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: userlists_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.userlists_id_seq OWNED BY public.userlists.id;
-
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: -
---
-
-CREATE TABLE public.users (
-    id integer NOT NULL,
-    username character varying(255) NOT NULL,
-    password character varying(255) NOT NULL,
-    email character varying(255) NOT NULL
+-- Create listplants table
+CREATE TABLE listplants (
+    id INTEGER PRIMARY KEY DEFAULT nextval('listplants_id_seq'),
+    list_id INTEGER,
+    plant_id INTEGER,
+    FOREIGN KEY (list_id) REFERENCES userlists(id) ON DELETE CASCADE,
+    FOREIGN KEY (plant_id) REFERENCES plants(id)
 );
-
-
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: -
---
-
-CREATE SEQUENCE public.users_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
---
-
-ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
-
-
---
--- Name: listplants id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.listplants ALTER COLUMN id SET DEFAULT nextval('public.listplants_id_seq'::regclass);
-
-
---
--- Name: plants id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.plants ALTER COLUMN id SET DEFAULT nextval('public.plants_id_seq'::regclass);
-
-
---
--- Name: userlists id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.userlists ALTER COLUMN id SET DEFAULT nextval('public.userlists_id_seq'::regclass);
-
-
---
--- Name: users id; Type: DEFAULT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
-
-
---
--- Name: listplants listplants_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.listplants
-    ADD CONSTRAINT listplants_pkey PRIMARY KEY (id);
-
-
---
--- Name: plants plants_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.plants
-    ADD CONSTRAINT plants_pkey PRIMARY KEY (id);
-
-
---
--- Name: userlists userlists_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.userlists
-    ADD CONSTRAINT userlists_pkey PRIMARY KEY (id);
-
-
---
--- Name: users users_email_key; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_email_key UNIQUE (email);
-
-
---
--- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.users
-    ADD CONSTRAINT users_username_key UNIQUE (username);
-
-
---
--- Name: listplants listplants_list_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.listplants
-    ADD CONSTRAINT listplants_list_id_fkey FOREIGN KEY (list_id) REFERENCES public.userlists(id) ON DELETE CASCADE;
-
-
---
--- Name: listplants listplants_plant_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.listplants
-    ADD CONSTRAINT listplants_plant_id_fkey FOREIGN KEY (plant_id) REFERENCES public.plants(id);
-
-
---
--- Name: userlists userlists_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
---
-
-ALTER TABLE ONLY public.userlists
-    ADD CONSTRAINT userlists_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
-
-
---
--- PostgreSQL database dump complete
---
-
